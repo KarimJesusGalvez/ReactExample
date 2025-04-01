12e6d643f472978eb161b19e175463548a9361da
KJAMRAOUI <KarimJesus.AmraouiGalvez@ust.com>
2025-04-01 11:08:24 +0200

    Added App.js

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+export default function App() {
+  return (
+    <div id="Appcontainer">
+        <header>
+            <span>Header</span>
+        </header>
+        <main>
+            <section id="1stsection">1st section</section>
+        </main>
+        <footer>
+            <span>Footer</span>
+        </footer>
+    </div>
+  );
+}
