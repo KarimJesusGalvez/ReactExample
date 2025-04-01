89a8e71bbb2c6b6fb6d045764f738f30c708a50c
KJAMRAOUI <KarimJesus.AmraouiGalvez@ust.com>
2025-04-01 18:03:26 +0200

    Added Router and pages to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,23 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./home/HomePage";
+import Header from "./common/Header";
+import PageNotFound from "./Errors/pageNotFound/PageNotFound";
+import ErrorFallback from "./Errors/ReactError/ReactErrorPage";
+import RenderError from "./Errors/ReactError/ErrorComponent";
+
 export default function App() {
+
+
   return (
     <div id="Appcontainer">
-        <header>
-            <span>Header</span>
-        </header>
-        <main>
-            <section id="1stsection">1st section</section>
-        </main>
-        <footer>
-            <span>Footer</span>
-        </footer>
+      <Header />
+      <ErrorFallback fallback={<RenderError></RenderError>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </ErrorFallback>
     </div>
   );
 }
