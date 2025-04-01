32de3d31def9cfd10385c4f1ca9ddd16db503d8e
KJAMRAOUI <KarimJesus.AmraouiGalvez@ust.com>
2025-04-01 18:00:05 +0200

    Added Header component

---
+++ src/components/common/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css"
+const Header = () => {
+    return (
+        <div id="HeaderContainer">
+            <button>ReColor</button>
+            <br/>
+            <button>Order</button>
+            <br/>
+            <button>Restore</button>
+        </div>
+    );
+};
+
+export default Header;
