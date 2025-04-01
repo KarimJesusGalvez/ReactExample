870c975693725c497a9e71ab3cbaf8fd0a5702b2
KJAMRAOUI <KarimJesus.AmraouiGalvez@ust.com>
2025-04-01 18:00:22 +0200

    Added 404 component

---
+++ src/components/Errors/pageNotFound/PageNotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./PageNotFound.css";
+
+const PageNotFound = () => {
+  return (
+    <div id="Error404PageContainer">
+      <h2>Error 404</h2>
+      <p>Page not found</p>
+    </div>
+  );
+};
+
+export default PageNotFound;
