ce3e2f78cf1c9bbcbf79363d372e0ddc01a9a7dd
KJAMRAOUI <KarimJesus.AmraouiGalvez@ust.com>
2025-04-02 11:56:20 +0200

    Added PersonreducerDelete

--- src/redux/reducers/PeopleReducer.js
+++ src/redux/reducers/PeopleReducer.js
@@ -1,13 +1,21 @@
-import { DELETE } from "../actions/actionTypes"
+import { DELETE } from "../actions/actionTypes";
+import initial_state from "../store/store_InitialState";
 
+function reducerDelete(state, action) {
+  let new_state = [];
+  // TODO use filter
+  Object.keys(state).forEach((keys) => {
+    if (state[keys].id !== action.person.id) new_state.push(state[keys]);
+  });
 
-function reducerDelete (){
- // TODO
+  return new_state;
 }
 
-export default function personReducer(state = [], action) {
-    switch (action.type) {
-        case DELETE: return reducerDelete(state, action);
-    default: return state
-    }
+export default function personReducer(state = initial_state(), action) {
+  switch (action.type) {
+    case DELETE:
+      return reducerDelete(state, action);
+    default:
+      return state;
+  }
 }
