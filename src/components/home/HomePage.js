03b3c2c8714da831c9e6a9ea7f106fb85db8037d
KJAMRAOUI <KarimJesus.AmraouiGalvez@ust.com>
2025-04-02 08:31:43 +0200

    Added css to home components

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import PeopleTable from "../PeopleList/peopleTable";
-
+import "./HomePage.css"
 const HomePage = () => {
 
     const people = useSelector((state) => state.people);
 
     return (
-    <div>
+    <div id="HomePageContainer">
         <h1>Home</h1>
         <div>{<PeopleTable></PeopleTable>}</div>
     </div>
