a2977f140cb20ea1632ec421d5855231d12fc158
KJAMRAOUI <KarimJesus.AmraouiGalvez@ust.com>
2025-04-02 17:17:25 +0200

    Added error if no state change in delete reducer

--- src/redux/reducers/PeopleReducer.js
+++ src/redux/reducers/PeopleReducer.js
@@ -4,9 +4,14 @@ import initial_state from "../store/store_InitialState";
 function reducerDelete(state, action) {
   let new_state = [];
   // TODO use filter
+
   Object.keys(state).forEach((keys) => {
-    if (state[keys]._gen_id !== action.person._gen_id) new_state.push(state[keys]);
+    if (state[keys]._gen_id !== action.person._gen_id) {
+      new_state.push(state[keys]);
+    }
   });
+  if (Object.keys(state).length === Object.keys(new_state).length) 
+    throw Error("New State is equal")
 
   return new_state;
 }
